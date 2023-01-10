36163f785f3e3a4cfc8aed15f0c0c4ce86455c3f
Blaez <ddltzhm@hotmail.com>
2023-01-10 09:07:25 -0500

    These were unrevioned?

---
+++ The Prototype/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Console.WriteLine("Pilot, enter a number between 0 and 100.");
+// int pilotnumber = Convert.ToInt32(Console.Read());
+
+// while (true)
+// {
+//     if (pilotnumber >=0 && pilotnumber <=100)
+//     {
+//         Console.WriteLine("Invalid Number try again.");   
+//     break;
+//     }
+//     Console.WriteLine("Hunter, guess the pilot's number.");
+//     int hunternumber = Convert.ToInt32(Console.Read());
+// }
+
+
+// while (true)
+// {
+//     Console.WriteLine("Pilot, enter a number between 0 and 100.");
+//     int pilotnumber = Convert.ToInt32(Console.ReadLine());
+//
+//     if (pilotnumber <= 0 || pilotnumber >= 100)
+//     {
+//         Console.Clear();
+//         Console.WriteLine("Invalid, Choose a different number.");
+//         break;
+//     }
+//
+//     for (pilotnumber <=0 || pilotnumber >= 100)
+//     {
+//         if (pilotnumber >= 0 || pilotnumber <= 100)
+//         {
+//             Console.Clear();
+//     
+//             Console.WriteLine("Hunter guess the pilot's number.");
+//             int hunternumber = Convert.ToInt32(Console.ReadLine());
+//
+//             if (pilotnumber < hunternumber)
+//             {
+//                 Console.WriteLine("Number is too high try again.");
+//                 continue;
+//             }
+//
+//             if (pilotnumber > hunternumber)
+//             {
+//                 Console.WriteLine("Number is too low, try again.");
+//                 continue;
+//             }
+//
+//             if (hunternumber == pilotnumber)
+//             {
+//                 Console.WriteLine("You guessed correctly!");
+//                 break;
+//             }
+//         }
+//     }
+
+
