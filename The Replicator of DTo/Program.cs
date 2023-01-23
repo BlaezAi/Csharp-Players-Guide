1bc0887f96a1b187b8df516bb8fb158c270c47f9
Blaez <ddltzhm@hotmail.com>
2023-01-23 11:39:44 -0500

    Finished Arrays

--- The Replicator of DTo/Program.cs
+++ The Replicator of DTo/Program.cs
@@ -1,5 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int[] length = {Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine()),};
- 
- Console.WriteLine(length);
+int[] userInputArray = new int[5];
+Console.WriteLine("Type in numbers to the replicator.");
+for (int i = 0; i < userInputArray.Length; i++)
+{
+    Console.Write("Enter numbers ");
+    userInputArray[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int[] copiedArray = new int[5];
+for (int i = 0; i < userInputArray.Length; i++)
+{
+    copiedArray = userInputArray;
+}
+
+for (int i = 0; i < userInputArray.Length; i++)
+{
+    Console.WriteLine($"{userInputArray[i]} and {copiedArray[i]}");
+}
